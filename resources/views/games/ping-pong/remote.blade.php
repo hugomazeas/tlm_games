6be8fe791f801c03cc9b037be5c31baaae5c20b2
root <root@b2a21eeb9b05>
2026-03-13 15:46:58 +0000

    agent: Double the polling rates of remotes.

--- resources/views/games/ping-pong/remote.blade.php
+++ resources/views/games/ping-pong/remote.blade.php
@@ -405,7 +405,7 @@
 
         function startPolling() {
             stopPolling();
-            pollTimer = setInterval(pollMatch, 2000);
+            pollTimer = setInterval(pollMatch, 1000);
         }
 
         function stopPolling() {
